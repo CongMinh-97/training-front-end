92085b641788a1eef6139e814b143d919d7d44ee
DungDoktpm <dungdoktpm@gmail.com>
2020-11-19 09:44:46 +0700

    page home

---
+++ src/assets/js/swiper.js
@@ -0,0 +1,64 @@
+var swiper01 = new Swiper('.header__banner .swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    }, 
+  });
+
+  var swiper02 = new Swiper('.list-box.list-box01 .swiper-container', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+  var swiper03 = new Swiper('.list-box.list-box02 .swiper-container', {
+    slidesPerView: 5,
+    spaceBetween: 30,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 4,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 50,
+      },
+    },
+  });
+
+  console.log(swiper02)
