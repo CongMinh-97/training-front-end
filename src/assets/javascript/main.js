4acbf214abec54277363c175a7d3f29d385758b4
DungDoktpm <dungdoktpm@gmail.com>
2020-11-21 10:45:09 +0700

    Remove src'

--- src/assets/javascript/main.js
+++
@@ -1,8 +0,0 @@
-let menu = document.querySelector('.header__menu');
-let btn_toggle = document.querySelector('.btn--toggle');
-btn_toggle.addEventListener('click', function(){
-    menu.classList.toggle('show');
-    menu.classList.toggle('show-menu');
-    menu.style.transition = ' all 0.5s';
-    console.log('click');
-});
