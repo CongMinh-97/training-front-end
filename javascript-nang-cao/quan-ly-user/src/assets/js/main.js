72cca5c3e66acd56367d35c212d95604e39f4154
DungDoktpm <dungdoktpm@gmail.com>
2020-11-29 21:12:05 +0700

    fix validate form

--- javascript-nang-cao/quan-ly-user/src/assets/js/main.js
+++ javascript-nang-cao/quan-ly-user/src/assets/js/main.js
@@ -29,7 +29,20 @@ class User {
 
     create(){
         
-        // Validator();
+       Validator({
+            form: "#form-data",
+            rules:[ 
+                Validator.isRequired("#name"),
+                Validator.isRequired("#age"),
+                Validator.isRequired("#email"),
+                Validator.isRequired("#address"),
+                Validator.isNumber("#age"),
+                Validator.isEmail("#email"),
+            ],
+            onSubmit: function(data){
+                return data;
+            }
+        });
         let newStudent = {
             name: this.userName.value,
             age: this.age.value,
@@ -55,14 +68,14 @@ class User {
     };
     this.data.splice(id,0,updateStudent);
     this.saveLocalStorage(this.data);
-    console.log(this.data);
+    // console.log(this.data);
     this.init();
    }
 
    destroy(id){
     this.data.splice(id, 1);
     this.saveLocalStorage(this.data);
-    console.log(this.data);
+    // console.log(this.data);
     this.init();
    }
 
@@ -72,31 +85,108 @@ var user = new User();
 user.init();
 
 function Validator(options){ 
-    // console.log(options) 
+    // hafm xử lý validate cho 1 inputElement 
+    function validate(inputElement, rule) {
+        var errorElement = inputElement.parentElement.querySelector('.message-error');// lấy element hiển thị lỗi
+        // var errorMessage = rule.test(inputElement.value);
+        var errorMessage;
+        //lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+        // lặp qua từng rule
+        for(let i=0; i<rules.length; i++){
+            errorMessage = rules[i](inputElement.value);
+            //ktra rule, nếu có lỗi thì dừng việc kiểm tra rule tiếp theo của chính selector đó
+            if (errorMessage) {
+               break;
+            }
+        }
+        if(errorMessage){
+            errorElement.style.display = 'block';
+            errorElement.style.color = 'red';
+            errorElement.innerText =errorMessage;                        
+        }
+        else errorElement.innerText ='';
+        return !errorMessage; // convert thanh dang boolean: true or false
+
+    }
+
+    var selectorRules= {};
     var formElement = document.querySelector(options.form);// lấy element của form  cần validate
     if(formElement){
-        // console.log(options.rules)
+        //khi submit form
+        formElement.onsubmit = function(e){
+            e.preventDefault();
+            var isFormInvalid = true;
+            //lặp qua từng rule và validate
+            options.rules.forEach(rule=> {
+                var inputElement = formElement.querySelector(rule.selector);
+                var isValid = validate(inputElement, rule);
+                if(!isValid){
+                    isFormInvalid = false;
+                }
+            });
+            if (isFormInvalid) {
+                if (typeof options.onSubmit === 'function') {
+                    var enableInputs = formElement.querySelectorAll('[name]');
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        
+                        switch(input.type) {
+                            case 'radio':
+                                values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
+                                break;
+                            case 'checkbox':
+                                if (!input.matches(':checked')) {
+                                    values[input.name] = '';
+                                    return values;
+                                }
+                                if (!Array.isArray(values[input.name])) {
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;
+                            default:
+                                values[input.name] = input.value;
+                        }
+                        return values;
+                        
+                    }, {});
+                    console.log(formValues);
+                    options.onSubmit(formValues);
+                    
+                }
+                // Trường hợp submit với hành vi mặc định
+                else {
+                    console.log('sbumit');
+                    formElement.submit();
+                }
+            }
+        }
+        // lặp qua mỗi rule của form và xử lý (lắng nghe các sự kiện: blur, click...)
         options.rules.forEach(rule => {
+            // lưu lại các rules trong mỗi input          
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            }else {
+                selectorRules[rule.selector]= [rule.test];
+            }
             var inputElement = formElement.querySelector(rule.selector);
-            var errorElement = inputElement.parentElement.querySelector('.message-error');// lấy element hiển thị lỗi
             if(inputElement){
                 // bat su kien blur khoi input
                 inputElement.onblur = function(){
-                    var errorMessage = rule.test(inputElement.value);
-                    if(errorMessage){
-                        errorElement.style.display = 'block';
-                        errorElement.style.color = 'red';
-                        errorElement.innerText =errorMessage;                        
-                    }
-                    else errorElement.innerText ='';
+                    validate(inputElement, rule);
                 }
                 // bắt sự kiện ng dùng nhập vào input thì k hiện message lỗi nữa, gặp lỗi -> báo
                 inputElement.oninput = function(){
+                    var errorElement = inputElement.parentElement.querySelector('.message-error');
                     errorElement.innerText ='';
                     errorElement.style.display = 'none';
                 }
             };
         });
+        // console.log(selectorRules);
     }   
 }
 Validator.isRequired = function(selector){
